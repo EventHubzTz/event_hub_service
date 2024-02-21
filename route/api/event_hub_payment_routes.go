4d0f16e3f4319bd696eb2b17493dc7362d184293
Gregory Mjange <gregorymjange2000@gmail.com>
2024-02-21 11:22:50 +0300

    Update Payment Status

--- route/api/event_hub_payment_routes.go
+++ route/api/event_hub_payment_routes.go
@@ -6,7 +6,10 @@ import (
 )
 
 func EventHubPaymentRoutes(route fiber.Router) {
+	route.Post("/Checkout/Callback", controllers.EventHubPaymentController.UpdatePaymentStatus)
+}
+
+func AuthenticatedEventHubPaymentRoutes(route fiber.Router) {
 	route.Post("/push/ussd", controllers.EventHubPaymentController.PushUSSD)
 	route.Post("/get/payment/transaction", controllers.EventHubPaymentController.GetPaymentTransactions)
-	route.Post("/Checkout/Callback", controllers.EventHubPaymentController.UpdatePaymentStatus)
 }
