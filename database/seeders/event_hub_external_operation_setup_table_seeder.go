7f3a241e2b9a67d363cf2fc45cae7b775f5a32f7
Gregory Mjange <gregorymjange2000@gmail.com>
2024-02-28 10:23:51 +0300

    Seeders

--- database/seeders/event_hub_external_operation_setup_table_seeder.go
+++ database/seeders/event_hub_external_operation_setup_table_seeder.go
@@ -7,14 +7,14 @@ import (
 
 func EventHubExternalOperationSetupTableSeeder(db *gorm.DB) error {
 	parameter1 := models.EventHubExternalOperationsSetup{Parameter: "Firebase FCM One Signal Notification URL", Value: "https://onesignal.com/api/v1/notifications"}
-	parameter2 := models.EventHubExternalOperationsSetup{Parameter: "Next SMS Sender ID", Value: "EVENT HUB"}
-	parameter3 := models.EventHubExternalOperationsSetup{Parameter: "Next SMS Single Destination Message URL", Value: "https://messaging-service.co.tz/api/sms/v1/text/single"}
-	parameter4 := models.EventHubExternalOperationsSetup{Parameter: "Next SMS Authorization token", Value: "Yl9ta2luZGk6Qm1AOTA4OTM="}
-	parameter5 := models.EventHubExternalOperationsSetup{Parameter: "Azampay Authenticator Base Url", Value: "https://authenticator-sandbox.azampay.co.tz"}
-	parameter6 := models.EventHubExternalOperationsSetup{Parameter: "Azampay Checkout Base Url", Value: "https://sandbox.azampay.co.tz"}
-	parameter7 := models.EventHubExternalOperationsSetup{Parameter: "Azampay App Name", Value: "EVENT HUB"}
-	parameter8 := models.EventHubExternalOperationsSetup{Parameter: "Azampay Client ID", Value: "a0432bca-6253-4592-a687-ce5c5e02408d"}
-	parameter9 := models.EventHubExternalOperationsSetup{Parameter: "Azampay Client Secret", Value: "NSZogwbovVT45OvAx1D3pbkCX28sEltJPHbl5iO5bvUpB9FwFeSR9xRk5SH5eMjAArAN2nxnrYqRVuEhjrLrWnfrP5jAC605h+mw1P3X+0WtKdLGZYezgJ80VctKodEndt70a52aGxTZs9lkJlfHliYCizTRjcqT0Awjvi7Zn7zESpgNXUIOgL+dP9ei6YVMHKD3GHkbv2rnkiQVbaDBG25AcQzh/amCIhmniCRJ2o4ADVvPEpizy3mZDLMenPg57GIQej81lbds9anVa01WX7vXTnk2WULt4dUykFFctHEo8oPydm6HlBle6qWd1en4ORKiBr33QfFZUow7MWzVqk1CxSymcCmGRR5wXXEHzahALkHmfLKixzhacCCD6JJp7VdEVjbKo4ujdYWY0Cl6dxARcPs/UYxqYmRslYPWzLSucW+xSKQzVBvPABFQOLWK4xss2F85TetvOpTjqKLUzn0LqIHTkeRn2mnu4+1KLkq4Fxhq9WfSclvIlIbvqHWEbzgbXvTfI8hhAl60450x6+S1XUJrYjTrUTmSPXo3KtqnPNLiCzqvhclI7lxLcevGuZBTL988h3kslZUeMe/nZqI7nqS8iQmQt30sEfUvtDPLOhZR2gUUVyy/b+eLx4Oi/+EuB93TsgfjjLKHXwg06ivA7g0Qa/EcwWubRqfF2o4="}
+	parameter2 := models.EventHubExternalOperationsSetup{Parameter: "Next SMS Sender ID", Value: "ALECOtr"}
+	parameter3 := models.EventHubExternalOperationsSetup{Parameter: "Next SMS Single Destination Message URL", Value: "https://mshastra.com/sendurl.aspx "}
+	parameter4 := models.EventHubExternalOperationsSetup{Parameter: "Next SMS Authorization token", Value: "Bm@90893$ "}
+	parameter5 := models.EventHubExternalOperationsSetup{Parameter: "Azampay Authenticator Base Url", Value: "https://authenticator.azampay.co.tz"}
+	parameter6 := models.EventHubExternalOperationsSetup{Parameter: "Azampay Checkout Base Url", Value: "https://checkout.azampay.co.tz"}
+	parameter7 := models.EventHubExternalOperationsSetup{Parameter: "Azampay App Name", Value: "EVENT HUB TZ"}
+	parameter8 := models.EventHubExternalOperationsSetup{Parameter: "Azampay Client ID", Value: "2d648311-1eba-4b1b-8e4d-b080b33046e9"}
+	parameter9 := models.EventHubExternalOperationsSetup{Parameter: "Azampay Client Secret", Value: "Wp4QhqcKNONluPNojE2VqsJUJS2LWnFbOmHN1rUsJsltWgIcvSoNOyX4AVZdKwi9te/ypTXmq2AX10FRCH52kDk8WdLHgyrjlkEcIutVYY25ZWJ2rvUWNGVOoJvvLw6Qx2OmZbL3ZyxRFcu2H3deQ2cUjlG4FwESu08bqvMTtJEpztP1MXDpqXq6PGuS/5ev/tk/X+V+0PQN13dtKvqF/5TJMVjM97chS7Usf9aKxAOL1yl3J0Fi9NO8+9hmxYdF9cyRlDKb5aBxy7Luz139xfQ1SqeuLAJEtM7ZegPkSmEHt3RSrRema/JJ+bkZ0TFCR4pTWxYytd37PImYMo7wV4LUn2KiKrqqu8HjTJ13HAUMLLOrrxcUzXKkOvxY5diTVywj2jJLc8wHTbv22rMIdkHZCjle+vS2tZOUzKGr7O0BA4JMGUEooSg5KNbrepaI1B4yGw399R4eCCtL8evwvj1cQI+xgtzNP6RrWzYYR3lgHSr3/PQwy3LyHTdL+jcpl0QKxod04eq0gTSOGoULKGWdPorLWEZgoZUUoCaB6QpnJiEqTn78tGBNsnvFpHnxqFTOSoNOg0DlLw37bkimlcnlMJQO4H+4u/hhQviCnppeff1Q/hp6YpBkdtRsMI9B4u3nww/Ix2RjvqZU+lqo7ZS4j9lEZ/b25neHoVtAAZg="}
 	parameter10 := models.EventHubExternalOperationsSetup{Parameter: "Azampay API-Key Token", Value: "da9f7f5f-8251-4007-93a2-29b3d8992a20"}
 
 	allParameters := []models.EventHubExternalOperationsSetup{
