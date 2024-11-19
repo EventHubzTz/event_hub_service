54d3508cc9eb37a666d0df75c47cb6d712f969f4
root <root@server1.tuzozatamthilia.info>
2024-11-19 07:53:13 +0000

    Middleware modifications

--- app/http/middlewares/request_id_middleware.go
+++ app/http/middlewares/request_id_middleware.go
@@ -1,6 +1,9 @@
 package middlewares
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/EventHubzTz/event_hub_service/service"
 	"github.com/EventHubzTz/event_hub_service/utils"
 	"github.com/EventHubzTz/event_hub_service/utils/response"
@@ -9,13 +12,40 @@ import (
 
 func ApiRequestID(ctx *fiber.Ctx) error {
 	/*-----------------------------------------------
-	 01. GET VALUE OF THE 'Request-ID' HEADER
-	------------------------------------------------*/
-	requestID := ctx.Get("event-hub-sign-auth")
+	  01. GET VALUE OF THE 'Request-ID' HEADER
+	 ------------------------------------------------*/
+	var requestID string
+
+	// First, check if 'event-hub-sign-auth' header is present
+	requestID = ctx.Get("event-hub-sign-auth")
+	if requestID == "" {
+		// If 'event-hub-sign-auth' is not found, check 'Authorization' header
+		authHeader := ctx.Get("Authorization")
+		if strings.HasPrefix(authHeader, "event-hub-sign-auth: ") {
+			// Extract the REQ-ID from the Authorization header
+			requestID = strings.TrimPrefix(authHeader, "event-hub-sign-auth: ")
+			requestID = strings.TrimSpace(requestID) // Remove any leading/trailing whitespace
+		}
+	}
+
+	if requestID == "" {
+		// If requestID is still empty, return unauthorized response
+		return response.ErrorResponse("Missing Request ID", fiber.StatusUnauthorized, ctx)
+	}
+
 	utils.WarningPrint("REQ-ID: " + requestID)
+
+	// 1. Print Request Headers
+	headers := ctx.GetReqHeaders()
+	fmt.Printf("Request Headers: %+v\n", headers)
+
+	// 2. Print Raw Request Body
+	rawBody := ctx.Body()
+	fmt.Printf("Raw Request Body: %s\n", string(rawBody))
+
 	/*-------------------------------------------------
-	02. COMPARE TO THE ACTIVE REQUEST ID IN SYSTEM
-	--------------------------------------------------*/
+	  02. COMPARE TO THE ACTIVE REQUEST ID IN SYSTEM
+	 --------------------------------------------------*/
 	status, err := service.EventHubRequestIDService.VerifyRequestID(requestID)
 	if err != nil {
 		return response.ErrorResponse(err.Error(), fiber.StatusUnauthorized, ctx)
@@ -23,5 +53,6 @@ func ApiRequestID(ctx *fiber.Ctx) error {
 	if !status {
 		return response.ErrorResponse("Unauthorized Access", fiber.StatusUnauthorized, ctx)
 	}
+
 	return ctx.Next()
 }
