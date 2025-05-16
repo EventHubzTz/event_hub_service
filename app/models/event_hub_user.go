92c7045e222e841a34dde00fb62d2d13ba6dc780
Gregory Mjange <gregorymjange2000@gmail.com>
2025-05-17 00:30:11 +0300

    Roles modification

--- app/models/event_hub_user.go
+++ app/models/event_hub_user.go
@@ -4,11 +4,11 @@ type EventHubUser struct {
 	ID
 	FirstName     string `json:"first_name" gorm:"not null;size:50" validate:"min=3"`
 	LastName      string `json:"last_name" gorm:"not null;size:50" validate:"min=3"`
-	Email         string `json:"email" gorm:"null;size:50" validate:"email,unique=users_tests.email"`
-	PhoneNumber   string `json:"phone_no" gorm:"not null;unique;size:20" validate:"required,min=9,unique=users_tests.phone_number"`
+	Email         string `json:"email" gorm:"null;unique;size:50"`
+	PhoneNumber   string `json:"phone_no" gorm:"not null;unique;size:20"`
 	Gender        string `json:"gender" gorm:"not null;type:enum('MALE','FEMALE');default:'MALE'"`
 	ProfileImage  string `json:"profile_image" gorm:"null;size:200;default:/users/profileImages/male.png"`
-	Role          string `json:"role" gorm:"not null;type:enum('NORMAL_USER','EVENT_PLANNER','SUPER_ADMIN');default:'NORMAL_USER'"`
+	Role          string `json:"role" gorm:"not null;type:enum('NORMAL_USER','EVENT_PLANNER','ACCOUNTANT','SUPER_ADMIN');default:'NORMAL_USER'"`
 	Password      string `json:"password" gorm:"not null"`
 	ImageStorage  string `json:"image_storage" gorm:"not null;type:enum('LOCAL','REMOTE');default:'LOCAL'"`
 	IsValidEmail  bool   `json:"is_valid_email" gorm:"not null;default:false"`
